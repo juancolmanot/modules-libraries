371fff2218141f171c803a5926e6fbcbca034b8c
Juan <juancolmanot@gmail.com>
2023-04-25 16:35:11 -0300

    25/04/2023 - 16:35

--- modulosc/intermittency.c
+++ modulosc/intermittency.c
@@ -22,3 +22,25 @@ bool ejection(double xn1, double xn, double ubr, double lbr) {
         return false;
     }
 }
+
+/* Two dimensional map from Kim & Kye */
+
+void map_2d(double* x1, double* x, double alpha, double beta) {
+    
+    x1[0] = 4.f * alpha * x[0] * (1.f - x[0]) + beta * x[1] * (1.f - x[0]);
+    x1[1] = 4.f * alpha * x[1] * (1.f - x[1]) + beta * x[0] * (1.f - x[1]);
+}
+
+void map_2d_n(double* x1, double* x, unsigned int n, double alpha, double beta) {
+
+    double* x_aux = malloc(2 * sizeof(double));
+    
+    x_aux[0] = x[0];
+    x_aux[1] = x[1];
+
+    for (unsigned int i = 0; i < n; i++){
+        map_2d(x1, x_aux, alpha, beta);
+        x_aux[0] = x1[0];
+        x_aux[1] = x1[1];
+    }
+}
--- modulosc/intermittency.h
+++ modulosc/intermittency.h
@@ -7,4 +7,9 @@ bool reinjection(double xn1, double xn, double ubr, double lbr);
 
 bool ejection(double xn1, double xn, double ubr, double lbr);
 
+/* Two dimensional map from Kim & Kye */
+void map_2d(double* x1, double* x, double alpha, double beta);
+
+void map_2d_n(double* xn1, double* xn, unsigned int n, double alpha, double beta);
+
 #endif
